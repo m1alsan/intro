40b9bf95ecdfdbb7fbf8e1a6d50000ba1d25ad62
m1alsan <m1alsan@yahoo.com>
2020-12-23 16:09:02 +0300

    Update noisy_terrain.js

--- noisy_terrain.js
+++ noisy_terrain.js
@@ -1,7 +1,18 @@
 function setup(){
-createCanvas(400, 400);
-background('black');
+  createCanvas(600, 600);
+  background('black');
+  noStroke();
 }
 
 function draw(){
+}
+
+function draw_terrain(){
+  for(var x=100;x<500;x=x+5){
+for(var y=100;y<500;y=y+5;){
+
+
+circle(x,y,2);
+}
+  }
 }
