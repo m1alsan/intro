037c737373ca46a43218b322ed5d796607760925
m1alsan <m1alsan@yahoo.com>
2020-12-24 16:28:54 +0300

    Update noisy_terrain.js

--- noisy_terrain.js
+++ noisy_terrain.js
@@ -1,3 +1,6 @@
+let lastTime=0;
+let deltaTime=1000;
+
 function setup(){
   createCanvas(950, 600);
   background('black');
@@ -6,26 +9,38 @@ function setup(){
 }
 
 function draw(){
-  draw_terrain(random(-20,20));
+  draw_terrain(random(-1,1));
+
+  if(deltaTime <= millis()-lastTime){
+    lastTime=millis();
+    //clear();
+
+    //text('Milliseconds \nrunning: \n' + lastTime, 5, 40*lastTime/500);
+  }
+
+
+
 }
 
 function mousePressed() {
   clear();
+  background('black');
 }
 
 function draw_terrain(r){
   for(var x=100;x<500;x=x+10){
     for(var y=100;y<500;y=y+10){
-			
-			let x2=x+y/2+r;
+
+			let x2=x+y/2;
 			let h=200*noise(x/200,y/200);
 			h+=30*noise(x/50,y/50);
-			let y2=y+h-100+r;
-			
-			
+			let y2=y+h-100;
+
+
       fill((map(h,0,230,0,360)+120)%360,80,map(h,0,230,100,50),90);
+
       circle(x2,y2,2);
-      
+
     }
   }
 }
